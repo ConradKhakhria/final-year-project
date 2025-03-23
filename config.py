7424b3cbe7012f13e401694a607c14ae1de68f09
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-03-23 17:50:08 +0000

    added debug()

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
 CHECK_CUDA = True
 DEBUG = True
 MODEL = "mistralai/Mistral-7B-Instruct-v0.3"
+
+
+# Some helper functions
+
+def debug(msg: str):
+    """
+    Prints the debug message, if DEBUG == True
+    """
+
+    global DEBUG
+
+    if DEBUG:
+        print(f"[debug]: {msg}")
