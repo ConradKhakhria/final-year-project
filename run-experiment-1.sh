3895a125f861fb96647e65c0a8ec288cf64c56ee
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-03-24 17:10:20 +0000

    created new run script

---
+++ run-experiment-1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -N fyp-gpu
+#$ -cwd
+#$ -l h_rt=01:00:00
+#$ -l gpu=1
+#$ -l mem=8G
+#$ -pe smp 4
+#$ -o logs/$JOB_NAME_$JOB_ID.out
+#$ -e logs/$JOB_NAME_$JOB_ID.err
+
+# Load required modules
+module -f unload compilers mpi gcc-libs
+module load beta-modules
+module load gcc-libs/10.2.0
+module load python3/3.9-gnu-10.2.0
+module load cuda/11.3.1/gnu-10.2.0
+module load cudnn/8.2.1.32/cuda-11.3
+module load pytorch/1.11.0/gpu
+
+# Optional: activate virtualenv or conda
+# source env/bin/activate
+
+# Run the script
+python experiment-1.py
