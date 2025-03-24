0daf969380bc9453b6e163162c98472fa5d44e5a
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-03-24 16:00:33 +0000

    wheel

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,3 +1,4 @@
 python3.11 -m pip install torch==2.1.2+cu118 torchvision==0.16.2+cu118 torchaudio==2.1.2   --index-url https://download.pytorch.org/whl/cu118;
+python3.11 -m pip install wheel;
 python3.11 -m pip install -r requirements.txt;
 python3.11 -m pip install flash-attn --no-build-isolation
