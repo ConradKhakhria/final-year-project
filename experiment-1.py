7154fcb93fe6726d5bf90e94fbcefa983f5b70e5
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-03-24 17:26:58 +0000

    changed experiment 1

--- experiment-1.py
+++ experiment-1.py
@@ -15,4 +15,37 @@ if __name__ == "__main__":
     df, X, y = dataset.load_dataset_locally("blog_authorship_corpus", "text", ["age", "gender"])
     m = model.SequenceModel(config.MODEL)
 
-    print(f"X[0] = {X[0]}")
+    m.set_pre_prompt("""You are an AI assistant which:
+1. Only receives un-annotated social media posts as input
+2. Only emits output in valid JSON format, with this schema:
+    {
+        "age": <your prediction of the age (**strictly** an integer)>,
+        "gender": <your prediction of the gender of the poster, either 'male' or 'female'>
+    }
+
+You will now receive a single input, and you must reply **only** in JSON, with no extra text.
+If you are unsure of the classification for age or gender, guess.""")
+
+    subset_size = 100
+    subset_offset = 100_000
+
+    X_test = X[subset_offset : subset_offset + subset_size]
+    y_test = y[subset_offset : subset_offset + subset_size]
+
+    outputs = m.query_sequence_batched(X_test, batch_size=16)
+    json_outputs = m.extract_json(outputs)
+
+    for i in range(len(outputs)):
+        y_true = y_test[i]
+        y_pred = json_outputs[i]
+
+        if y_pred:
+            print(f"""instance {i}:\n
+    - age:
+        pred: {y_pred["age"]}
+        true: {y_true["age"]}
+    - gender:
+        pred: {y_pred["gender"]}
+        true: {y_true["gender"]}""")
+        else:
+            print(f"Regrettably this is the output: {outputs[i]}")
