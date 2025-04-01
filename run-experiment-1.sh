beb1c686858aba6bfe647a245c62844976e59f76
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-01 23:03:32 +0100

    removed python 3.9

--- run-experiment-1.sh
+++ run-experiment-1.sh
@@ -18,7 +18,6 @@ module load python3/3.11
 module -f unload compilers mpi gcc-libs
 module load beta-modules
 module load gcc-libs/10.2.0
-module load python3/3.9-gnu-10.2.0
 module load cuda/11.3.1/gnu-10.2.0
 module load cudnn/8.2.1.32/cuda-11.3
 module load pytorch/1.11.0/gpu
