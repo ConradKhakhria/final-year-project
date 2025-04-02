91ac92c463539997acce84754e8425c71ff1e8b6
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-02 16:51:22 +0100

    added container test

---
+++ run-experiment-1-container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -N fyp-gpu-container
+#$ -cwd
+#$ -l h_rt=00:05:00
+#$ -l gpu=0
+#$ -l mem=2G
+#$ -pe smp 4
+#$ -o $HOME/Scratch/fyp/logs/$JOB_NAME_$JOB_ID.out
+#$ -e $HOME/Scratch/fyp/logs/$JOB_NAME_$JOB_ID.err
+
+module load apptainer
+
+apptainer exec --nv -B $HOME/ACFS/final-year-project:/project \
+    $HOME/ACFS/final-year-project/pytorch_container.sif \
+    python3 /project/experiment-1.py
