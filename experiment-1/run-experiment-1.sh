401c1fdda41fe0ff48f89578d2de53cb6eb5b016
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-04 16:49:43 +0100

    removed CPU line

--- experiment-1/run-experiment-1.sh
+++ experiment-1/run-experiment-1.sh
@@ -11,9 +11,6 @@
 source /etc/profile
 module load apptainer
 
-# comment this out if you're actually using the gpu
-module load pytorch/2.1.0/cpu
-
 echo "Running on node: $(hostname)"
 nvidia-smi || echo "No GPU detected"
 
