f7cbe80b70fe775aa88034e8f4988f0dec5a837c
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-04 12:02:45 +0100

    added import path

--- cache-dataset/cache_dataset.py
+++ cache-dataset/cache_dataset.py
@@ -1,6 +1,12 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.dirname(__file__), "..", "experiment-1")
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
 import config
 import dataset
-import sys
 
 
 if __name__ == "__main__":
