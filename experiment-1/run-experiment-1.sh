4c6dc127f32c43ce01346dd18351edf187ec2930
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-05 19:21:31 +0100

    added hf token as env variable

--- experiment-1/run-experiment-1.sh
+++ experiment-1/run-experiment-1.sh
@@ -15,6 +15,8 @@ module load apptainer
 echo "Running on node: $(hostname)"
 nvidia-smi || echo "No GPU detected"
 
+export HF_TOKEN=$(cat $HOME/ACFS/final-year-project/hf-access-token.txt)
+
 # run experiment
 apptainer exec --nv -B $HOME/ACFS/final-year-project:/project \
   $HOME/ACFS/final-year-project/experiment-container.sif \
