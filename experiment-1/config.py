a9a4c28607b39f6e269fe5a33e2cffebfbec8ddd
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-05 17:24:29 +0100

    changed location of storage dir (I'm going to kill myself)

--- experiment-1/config.py
+++ experiment-1/config.py
@@ -5,7 +5,7 @@ DEBUG = True
 MODEL = "mistralai/Mistral-7B-Instruct-v0.3"
 
 # directories
-LT_STORAGE_DIR = Path.home() / "ACFS" / "final-year-project"
+LT_STORAGE_DIR = Path("/project")
 LT_TEMPORARY_DIR = Path.home() / "Scratch" / "fyp"
 
 
