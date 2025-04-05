6ec4103416dffa3792445f6649b2542c5c9c6f44
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-05 19:13:05 +0100

    fixed import path

--- cache-dataset/cache_dataset.py
+++ cache-dataset/cache_dataset.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-current_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'experiment-1'))
+current_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
 
