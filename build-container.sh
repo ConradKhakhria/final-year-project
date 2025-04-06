2303afc91c91a3c8ccf152e33aaa5e9917fdc833
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-06 13:33:43 +0100

    test build

--- build-container.sh
+++ build-container.sh
@@ -26,7 +26,7 @@ echo "FAKEROOTKEY is set to $FAKEROOTKEY"
 # --bind: mounts $HOME/Scratch as /project inside the container
 # --tmpdir: uses your scratch space for temporary build files
 apptainer build \
-    --fakeroot -F --fix-perms --bind $HOME/Scratch:/project --tmpdir=$HOME/Scratch/tmp \
+    --fakeroot -F --bind $HOME/Scratch:/project --tmpdir=$HOME/Scratch/tmp \
     $HOME/Scratch/container/experiment-container.sif \
     $HOME/ACFS/final-year-project/experiment-container.def
 
---
+++ test-build/run-test-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -N test-apptainer
+#$ -cwd
+#$ -l h_rt=00:00:30
+#$ -l mem=2G
+#$ -pe smp 1
+#$ -o $HOME/Scratch/fyp/logs/$JOB_NAME_$JOB_ID.out
+#$ -e $HOME/Scratch/fyp/logs/$JOB_NAME_$JOB_ID.err
+
+# Load Apptainer
+source /etc/profile
+module load apptainer
+
+# Build the diagnostic container with fakeroot
+apptainer build --fakeroot --tmpdir=$HOME/Scratch/tmp $HOME/Scratch/container/test.sif $HOME/ACFS/final-year-project/test.def
+
+# Execute the container to see the diagnostic output
+apptainer exec --fakeroot $HOME/Scratch/container/test.sif
