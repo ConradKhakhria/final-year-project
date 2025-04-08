d365b5887dbe24b85a8998b8d25c70666e2a587e
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-08 18:39:36 +0100

    no cache

--- model.py
+++ model.py
@@ -174,7 +174,7 @@ class SequenceModel:
                     pad_token_id=self.tokenizer.eos_token_id,
                     do_sample=False,
                     repetition_penalty=1.1,
-                    use_cache=True,
+                    use_cache=False,
                 )
 
             torch.cuda.synchronize()
