29199d4e391b19b83d3e3ab7b72ef8c6d63e08f2
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-08 17:40:16 +0100

    fixed age storage

--- experiment-1.py
+++ experiment-1.py
@@ -35,6 +35,8 @@ def output_to_dataframe(y_pred: list[dict], y_true: list[dict]) -> pd.DataFrame:
     df_true = pd.DataFrame.from_records(y_true).rename(columns={"age": "true_age", "gender": "true_gender"})
     df_pred = pd.DataFrame.from_records(y_pred).rename(columns={"age": "pred_age", "gender": "pred_gender"})
 
+    df["pred_age"] = pd.to_numeric(df_pred["pred_age"], errors="coerce")
+
     df = pd.concat([df_true, df_pred], axis=1)
     df["valid_json"] = ~df["pred_age"].isna()
 
