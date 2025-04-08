3ba7366e3d4c71827ecb9043154b5d29f7ecf353
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-08 16:30:20 +0100

    added json again

--- experiment-1.py
+++ experiment-1.py
@@ -64,22 +64,14 @@ If you are unsure of the classification for age or gender, guess.""")
     X_test = X[subset_offset : subset_offset + subset_size]
     y_test = y[subset_offset : subset_offset + subset_size]
 
-    # just print outputs for now
+    # compute and write outputs
     outputs = m.query_sequence_batched(X_test, batch_size=16)
 
     with open(config.RESULTS_DIR / "experiment-1-out.txt", "w") as f:
         f.write("\n".join(outputs))
 
-    exit()
-
-
+    # parse json
     json_outputs = m.extract_json(outputs)
-
     results = output_to_dataframe(json_outputs, y_test)
-
-
-    # Record results
-    with open(config.RESULTS_DIR / "experiment-1-out.txt", "w") as f:
-        f.write("\n".join(outputs))
 
     results.to_parquet(config.RESULTS_DIR / "experiment-1-output.parquet", index=False)
