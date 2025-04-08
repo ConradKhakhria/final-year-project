c21fb1a33641e250ce4cf6a6d3fcd5492314131d
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-08 16:10:59 +0100

    increased batch size

--- experiment-1.py
+++ experiment-1.py
@@ -63,7 +63,7 @@ If you are unsure of the classification for age or gender, guess.""")
     X_test = X[subset_offset : subset_offset + subset_size]
     y_test = y[subset_offset : subset_offset + subset_size]
 
-    outputs = m.query_sequence_batched(X_test, batch_size=8)
+    outputs = m.query_sequence_batched(X_test, batch_size=16)
     json_outputs = m.extract_json(outputs)
 
     results = output_to_dataframe(json_outputs, y_test)
