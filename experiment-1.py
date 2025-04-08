7e223563d498a165bec142b600c2dc7f21a554d5
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-08 18:14:39 +0100

    smaller batch size

--- experiment-1.py
+++ experiment-1.py
@@ -153,7 +153,7 @@ if __name__ == "__main__":
     m.load_pre_prompt(config.CODE_DIR / "pre-prompts" / "expt1-zero-shot.txt")
 
     # compute and write outputs
-    outputs = m.query_sequence_batched(X_subset, batch_size=20)
+    outputs = m.query_sequence_batched(X_subset, batch_size=16)
 
     # parse json
     json_outputs = m.extract_json(outputs)
