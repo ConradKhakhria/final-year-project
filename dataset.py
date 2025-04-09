db72dca602aed28953a110c55f589032d81d8343
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-09 01:08:17 +0100

    this now only returns the df

--- dataset.py
+++ dataset.py
@@ -7,12 +7,7 @@ import config
 
 
 @config.debug_function
-def load_dataset_locally(
-    dataset_name: str,
-    X_name: str,
-    y_names: list[str],
-    sort_axis=None,
-    split_set="train") -> tuple:
+def load_dataset_locally(dataset_name: str, sort_axis=None, split_set="train") -> pd.DataFrame:
     """
     Loads a dataset from huggingface
 
@@ -27,9 +22,7 @@ def load_dataset_locally(
     - split_set: which of "train", "validation", and "test" you want
 
     Returns:
-    - df: the pandas DataFrame of the dataset
-    - X: list of values from the X column
-    - y: list of dicts with values from y columns
+        the pandas DataFrame of the dataset
     """
 
     dataset_cache_path = config.CACHE_DIR / "huggingface-datasets"
@@ -56,8 +49,4 @@ def load_dataset_locally(
         config.debug(f"Caching dataset locally to {local_dataset_path}")
         df.to_parquet(local_dataset_path)
 
-    config.debug("Selecting X and y columns")
-    X = df[X_name].tolist()
-    y = df[y_names].to_dict(orient="records")
-
-    return df, X, y
+    return df
