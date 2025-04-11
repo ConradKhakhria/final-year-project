804cc5832575dd08c5f3ff820347e0934ffa345d
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-11 15:39:18 +0100

    just on

--- experiment-1-comparison.py
+++ experiment-1-comparison.py
@@ -1,8 +1,10 @@
+# mypy: ignore-errors
 import datasets
 from joblib import Memory
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from sklearn.base import BaseEstimator
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.svm import LinearSVC, SVR
 from sklearn.ensemble import RandomForestClassifier, StackingClassifier
@@ -69,131 +71,108 @@ class DatasetLoader:
         return X, y
 
 
-# This class selects the best hyperparameters *for each model*
-class CVModelSelector:
-    def __init__(self):
-        self.vectorisers = {}
-        self.models = {}
+class CVParameterSelector:
+    def __init__(self, model):
+        self.model = model
+
         self.memory = Memory(location='./cache', verbose=0)
-
-        self.vectoriser_parameters = {}
-        self.model_parameters = {}
-
-        self.parameter_grid = []
+        self.hyperparameters = {}
+        self.vectorisers = {}
 
 
-    def add_vectoriser(self, name: str, vec, params: dict):
+    def add_parameters(self, name: str, values: list[float]):
         """
-        Adds a vectoriser to the model selector
+        Adds a hyperparameter and a set of candidate values
+        """
+        self.hyperparameters[name] = values
 
-        args:
-        - name: the name of the vectoriser
-        - vec: the vectoriser
-        - params: its tunably hyperparameters (dict)
+
+    def add_vectoriser(self, name: str, vec):
+        """
+        Adds a vectoriser
         """
         self.vectorisers[name] = vec
-        self.vectoriser_parameters[name] = params
 
 
-    def add_model(self, name: str, model, params: dict):
+    def grid_search(self, X, y, cv=5) -> dict:
         """
-        Adds a model to the model selector
+        Performs grid search CV on a dataset
 
         args:
-        - name: the name of the classifier
-        - model: the model
-        - params: its tunably hyperparameters (dict)
+        - X: the features to validate on
+        - y: the labels "" "" ""
+        - cv=5: the number of folds
+
+        returns:
+            a dict containing all of best hyperparameters
         """
-        self.models[name] = model
-        self.model_parameters[name] = params
+        parameter_grid = []
 
+        # Build hyperparameter grid
+        for vec_name, vec in self.vectorisers.items():
+            params = { "vec": [vec] }
 
-    def _build_parameter_grids(self):
-        """
-        Builds the parameter grids from the existing vectorisers/models
-        """
-        # Classifier parameter grid
-        for model_name, model in self.models.items():
-            for vec_name, vec in self.vectorisers.items():
-                params = { "kernel": [vec], "model": [model] }
+            for param, values in self.hyperparameters:
+                params[f"model__{param}"] = values
 
-                for param, values in self.vectoriser_parameters[vec_name].items():
-                    params[f"kernel__{param}"] = values
+            parameter_grid.append(params)
 
-                for param, values in self.model_parameters[model_name].items():
-                    params[f"model__{param}"] = values
-
-                self.parameter_grid.append(params)
-
-
-    def grid_search(self, X_train, y_train, cv=5) -> pd.DataFrame:
-        """
-        Performs grid search CV with cv folds
-        """
-        self._build_parameter_grids()
-
+        # grid search
         placeholder_pipeline = Pipeline([
-            ("kernel", list(self.vectorisers.values())[0]),
-            ("model", list(self.models.values())[0])
+            ("vectoriser", list(self.vectorisers.values())[0]),
+            ("model", self.model)
         ], memory=self.memory)
 
-
         config.debug("Grid search for classification")
-        grid_search = GridSearchCV(placeholder_pipeline, self.parameter_grid,
-                                   cv=5, n_jobs=-1, verbose=10)
-        grid_search.fit(X_train, y_train)
+        grid_search = GridSearchCV(placeholder_pipeline, parameter_grid, cv=5, n_jobs=-1, verbose=10)
+        grid_search.fit(X, y)
 
-        # find best models
-        results_df = pd.DataFrame(grid_search.cv_results_)
-
-        results_df['model_name'] = results_df['param_model'].apply(lambda x: type(x).__name__)
-        results_df['kernel_name'] = results_df['param_kernel'].apply(lambda x: type(x).__name__)
-
-        # Group by and select the best configuration based on mean_test_score.
-        best_per_group = results_df.loc[results_df.groupby(['model_name', 'kernel_name'])['mean_test_score'].idxmax()]
-
-        print("Best hyperparameters for each (model, vectoriser) combination:")
-        print(best_per_group[['model_name', 'kernel_name', 'mean_test_score', 'params']])
-
-        # Optionally, return the best configurations
-        return best_per_group
+        return grid_search.best_params_
 
 
+if False:
+    config.debug("Creating models and kernels")
 
-config.debug("Creating models and kernels")
+    # kernels
+    cls_model_selector = CVModelSelector()
 
-# kernels
-cls_model_selector = CVModelSelector()
+    cls_model_selector.add_vectoriser(
+        "tfidf",
+        TfidfVectorizer(max_features=5000, ngram_range=(1,2), stop_words="english"),
+        { "max_features": [1000, 3000] }
+    )
+    cls_model_selector.add_vectoriser(
+        "bofw",
+        CountVectorizer(max_features=5000, ngram_range=(1, 2), stop_words="english"),
+        { "max_features": [1000, 3000] }
+    )
 
-cls_model_selector.add_vectoriser(
-    "tfidf",
-    TfidfVectorizer(max_features=5000, ngram_range=(1,2), stop_words="english"),
-    { "max_features": [1000, 3000] }
-)
-cls_model_selector.add_vectoriser(
-    "bofw",
-    CountVectorizer(max_features=5000, ngram_range=(1, 2), stop_words="english"),
-    { "max_features": [1000, 3000] }
-)
-
-cls_model_selector.add_model("svm", LinearSVC(dual="False", max_iter=1000), { "C": [0.1, 1, 10] })
-cls_model_selector.add_model("lr", LogisticRegression(max_iter=1000), { "C": [0.1, 1, 10] })
-cls_model_selector.add_model(
-    "stack",
-    StackingClassifier(
-        estimators=[
-            ('lr', LogisticRegression(max_iter=1000)),
-            ('rf', RandomForestClassifier(n_estimators=100))
-        ],
-        final_estimator=LogisticRegression(max_iter=1000)
-    ),
-    { "final_estimator__C": [0.1, 1, 10] }
-)
+    cls_model_selector.add_model("svm", LinearSVC(dual="False", max_iter=1000), { "C": [0.1, 1, 10] })
+    cls_model_selector.add_model("lr", LogisticRegression(max_iter=1000), { "C": [0.1, 1, 10] })
+    cls_model_selector.add_model(
+        "stack",
+        StackingClassifier(
+            estimators=[
+                ('lr', LogisticRegression(max_iter=1000)),
+                ('rf', RandomForestClassifier(n_estimators=100))
+            ],
+            final_estimator=LogisticRegression(max_iter=1000)
+        ),
+        { "final_estimator__C": [0.1, 1, 10] }
+    )
 
 
 if __name__ == "__main__":
     dataset = DatasetLoader()
     X_train_cv, y_train_gender_cv = dataset.get_Xy("train", "gender", subset_size=50_000)
     
-    best_models = cls_model_selector.grid_search(X_train_cv, y_train_gender_cv)
-    best_models.to_parquet(PROJECT_PATH / "results" / "experiment-1-comparison-output.parquet")
+    lr_param_selector = CVParameterSelector(LogisticRegression(max_iter=1000))
+    lr_param_selector.add_vectoriser("tfidf", TfidfVectorizer(max_features=3000, ngram_range=(1,2),
+                                                              stop_words="english"))
+    lr_param_selector.add_vectoriser("bofw", CountVectorizer(max_features=3000, ngram_range=(1, 2), 
+                                                             top_words="english"))
+    lr_param_selector.add_parameters("C", [0.1, 1, 10])
+
+    best_params = lr_param_selector.grid_search(X_train_cv, y_train_gender_cv)
+
+    print(best_params)
