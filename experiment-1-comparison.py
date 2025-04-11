94367678bfaef98c344c7c96ea8a1510f7492777
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-11 21:00:52 +0100

    trying with 4 cores

--- experiment-1-comparison.py
+++ experiment-1-comparison.py
@@ -255,7 +255,7 @@ if __name__ == "__main__":
 
         best_rf = Pipeline([
             ("vectoriser", TfidfVectorizer(max_features=5000, ngram_range=(1, 2), stop_words='english')),
-            ("model", RandomForestClassifier(max_depth=None, n_estimators=200, n_jobs=-1)),
+            ("model", RandomForestClassifier(max_depth=None, n_estimators=200, n_jobs=4)),
         ])
 
         best_svm = Pipeline([
