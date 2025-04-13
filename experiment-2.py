ddcf0e8669f4a0eea32416237f3f5d794a3e906f
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-13 01:17:46 +0100

    now doesn't stop

--- experiment-2.py
+++ experiment-2.py
@@ -185,9 +185,7 @@ if __name__ == "__main__":
     for c in post_chunks:
         output = expt.get_trends_from_chunk(c)
 
-        print(output)
-
-        exit()
+        print("=====================\n" + output + "\n=================")
 
 
     """
