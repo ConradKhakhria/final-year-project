ef9dd15f7c8c0334a21264566bbe793990fccfba
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-14 18:05:32 +0100

    Added process isolator

--- experiment-2.py
+++ experiment-2.py
@@ -6,14 +6,121 @@ import os
 import pandas as pd
 from pathlib import Path
 import sys
+import torch
 from typing import Iterator, List, Literal, Tuple 
 
 import config
 import dataset
 import model
 
-LOCAL_TESTING = False
 NUM_SAMPLES = None
+
+
+class BatchModelIsolator:
+    def __init__(self, pre_prompt_name: str):
+        self.pre_prompt_path = config.CODE_DIR / "pre-prompts" / pre_prompt_name
+
+
+    def process_prompts(self, prompts: List[str], batch_size = None) -> List[str]:
+        """
+        Processes a list of prompts in a separate process
+
+        args:
+        - prompts: the list of prompts to process
+        - batch_size: the batch size (default to sequential)
+        """ 
+        outputs = []
+        batch_start = 0
+
+        if batch_size is None:
+            batch_size = 1
+
+        p, in_queue, out_queue = self.create_batch_process_worker()
+
+        while batch_start < len(prompts):
+            prompt_batch = prompts[batch_start : min(len(prompts), batch_start + batch_size)]
+
+            config.debug(f"Processing batch {batch_start}..{batch_start + batch_size} of {len(prompts)}")
+            in_queue.put(prompt_batch)
+
+            results = out_queue.get()
+            if results["successful"]:
+                outputs.extend(results["output"])
+                batch_start += batch_size
+            else:
+                if p.is_alive():
+                    p.terminate()
+                    p.join()
+
+                torch.cuda.empty_stack()
+
+                p, in_queue, out_queue = self.create_batch_process_worker()
+
+                new_batch_size = max(1, int(0.8 * batch_size))
+                if new_batch_size == batch_size > 1:
+                    new_batch_size -= 1
+
+                batch_size = new_batch_size
+
+        in_queue.put(None)
+        p.join()
+
+        return outputs
+
+
+    def create_batch_process_worker(self) -> Tuple[mp.Process, mp.Queue, mp.Queue]:
+        """
+        Creates a new batch process worker
+
+        returns:
+        A tuple containing:
+        1. The process
+        2. The input queue
+        3. The output queue
+        """
+        in_queue: mp.Queue = mp.Queue()
+        out_queue: mp.Queue = mp.Queue()
+
+        p = mp.Process(
+            target=self.batch_process_worker,
+            args=(in_queue, out_queue, self.pre_prompt_path)
+        )
+        p.start()
+
+        return p, in_queue, out_queue
+
+
+    @classmethod
+    def batch_process_worker(cls, in_queue: mp.Queue, out_queue: mp.Queue, pp_path: Path):
+        """
+        Creates a batch processing worker
+
+        args:
+        - input_queue: the queue this process take batches from
+        - output_queue: the queue this process writes output to, as dicts:
+            - "successful": whether the processing was successful (or OOM)
+            - "output": the list of text output from the model
+        - pp_path: pre-prompt path
+        """
+        m = model.BatchModel(config.SMALL_MODEL)
+        m.load_pre_prompt(pp_path)
+
+        with torch.no_grad():
+            while True:
+                if (batch := in_queue.get()) is None:
+                    break
+
+                try:
+                    output = m.process_batch(batch, enforce_json=True)
+                    out_queue.put({ "successful": True, "output": output })
+                except RuntimeError as e:
+                    if str(e).startswith('CUDA out of memory'):
+                        out_queue.put({ "successful": False, "output": None })
+                    else:
+                        raise e
+
+        torch.cuda.empty_cache()
+
 
 
 class Experiment2:
@@ -27,7 +134,7 @@ class Experiment2:
         with open(self.data_dir / "subreddit-selection.json") as f:
             self.subreddit_selection = json.load(f)
 
-        self.m = model.BatchModel(config.MODEL)
+        self.model_isolator = BatchModelIsolator("expt2-identify-trends-sector.txt")
 
 
     @config.debug_function
@@ -110,18 +217,8 @@ class Experiment2:
         returns:
             A string containing a bullet-pointed list of trends indicated
         """
-        self.m.set_pre_prompt(config.CODE_DIR / "pre-prompts" / "expt2-identify-trends-sector.txt")
-
         prompts = [self.chunk_to_prompt(c) for c in chunks]
-        outputs = []
-
-        if batch_size is None:
-            batch_size = 1
-
-        for batch_start in range(0, len(chunks), batch_size):
-            prompt_batch = prompts[batch_start : min(len(chunks), batch_start + batch_size)]
-            out = self.m.process_batch(prompt_batch, enforce_json=False, max_new_tokens=200)
-            outputs.extend(out)
+        outputs = self.model_isolator.process_prompts(prompts, batch_size=batch_size)
 
         return outputs
 
