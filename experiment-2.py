37894981d1859f79df680d2f2a7f49cf45517e25
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-14 16:38:08 +0100

    added relevant and irrelevant subs

--- experiment-2.py
+++ experiment-2.py
@@ -6,7 +6,7 @@ import os
 import pandas as pd
 from pathlib import Path
 import sys
-from typing import Iterator, Tuple
+from typing import Iterator, Literal, Tuple 
 
 import config
 import dataset
@@ -20,25 +20,38 @@ class Experiment2:
     def __init__(self):
         config.debug("Loading dataset")
 
-        data_path = config.CODE_DIR / "data" / "combined-filtered-reddit-data.parquet"
-
-        self.reddit_df = pd.read_parquet(data_path)
+        self.data_dir = config.CODE_DIR / "data"
+        self.reddit_df = pd.read_parquet(self.data_dir / "combined-filtered-reddit-data.parquet")
         self.reddit_df["date_posted"] = pd.to_datetime(self.reddit_df["created_utc"], unit="s")
 
         self.m = model.BatchModel(config.MODEL)
 
 
-    def sample_reddit_df(self, num_samples: int):
+    def select_test_df(
+        self, relevance: Literal["relevant", "irrelevant"], num_samples: int | None = None
+    ) -> pd.DataFrame:
         """
-        Selects a subset of size num_samples for reddit_df
+        Selects the testing df
+
+        args:
+        - relevance: whether to select the relevant or irrelevant subreddits
+        - num_samples: optional - whether to take a subset
         """
-        n_posts = len(self.reddit_df)
-        idxs = np.random.choice(np.arange(0, n_posts), size=num_samples, replace=False)
-        self.reddit_df = self.reddit_df[idxs]
+        with open(self.data_dir / "subreddit-selection.json") as f:
+            subreddit_selection = json.load(f)
+
+        test_df = self.reddit_df[self.reddit_df["subreddit"].isin(subreddit_selection[relevance])]
+
+        if num_samples is not None:
+            n_posts = len(test_df)
+            sample_idxs = np.random.choice(np.arange(0, n_posts), size=num_samples, replace=False)
+            test_df = test_df.iloc[sample_idxs]
+
+        return test_df
 
 
     @config.debug_function
-    def chunk_by_date_and_subreddit(self, chunk_size: int) -> Iterator[pd.DataFrame]:
+    def chunk_by_date_and_subreddit(self, df_test: pd.DataFrame, chunk_size: int) -> Iterator[pd.DataFrame]:
         """
         Iterates over dates and subreddits
 
@@ -46,11 +59,11 @@ class Experiment2:
         - df_test: the dataframe to divide into chunks
         - chunk_size: the size *in days* of each chunk
         """
-        start_date = self.reddit_df["date_posted"].min()
-        end_date = self.reddit_df["date_posted"].max()
+        start_date = df_test["date_posted"].min()
+        end_date = df_test["date_posted"].max()
 
-        time_sorted_df = self.reddit_df.sort_values(by="date_posted")
-        subreddits = self.reddit_df["subreddit"].unique()
+        time_sorted_df = df_test.sort_values(by="date_posted")
+        subreddits = df_test["subreddit"].unique()
 
         current_start = start_date
 
@@ -110,8 +123,8 @@ if __name__ == "__main__":
 
     expt = Experiment2()
 
-    if NUM_SAMPLES is not None:
-        expt.sample_reddit_df(NUM_SAMPLES)
+    relevant_df = expt.select_test_df("relevant", NUM_SAMPLES)
+    irrelevant_df = expt.select_test_df("irrelevant", NUM_SAMPLES)
 
     post_chunks = expt.chunk_by_date_and_subreddit(4)
 
@@ -124,8 +137,9 @@ if __name__ == "__main__":
     """
     Approach:
     1. Load (a subset of) reddit posts (submissions & comments)
-    2. filter subreddits with LLM
     3. Split up by time (4 days) and subreddit
-    4. For each time and subreddit, produce a report of all 
+    4. For each time and subreddit, produce a report of all consumer
+       trends (relating to Coca-Cola) indicated by the post
+    5. 
     """
 
