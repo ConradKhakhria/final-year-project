908402230146eddbc9ea9828f60a8ab6749b24ee
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-14 18:22:56 +0100

    5000 tokens

--- model.py
+++ model.py
@@ -111,7 +111,7 @@ class BatchModel:
             prompt_strings = [f"{self.pre_prompt}\n\n[input]: {i}\n[output]: " for i in batch]
 
         input_tokens = self.tokenizer(prompt_strings, return_tensors="pt", padding=True,
-                                      truncation=True, max_length=2000)
+                                      truncation=True, max_length=5000)
         input_tokens = input_tokens.to("cuda")
 
         output_tokens = self.model.generate(
