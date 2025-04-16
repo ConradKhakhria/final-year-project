c7260d808fa851df65f2b3742ad5e07d145fed0b
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-16 02:26:13 +0100

    fixed pentuple counting

--- experiment-2.py
+++ experiment-2.py
@@ -429,7 +429,7 @@ if __name__ == "__main__":
     mp.set_start_method("spawn")
 
     # Setup age and gender range
-    age_range = np.array([f"{5*(i // 5)}-{5*((i // 5) + 1)}" for i in range(100)] + ["unknown"])
+    age_range = np.array([f"{i}-{i + 5}" for i in np.arange(0, 100, 5)] + ["unknown"])
     gender_range = ["male", "female", "unknown"]
 
     expt = Experiment2(small_model_max_tokens=200, large_model_max_tokens=2000)
