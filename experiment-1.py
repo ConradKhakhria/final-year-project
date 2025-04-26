5ea3d580b2d5b61797a83e9166c94da575de53ea
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-26 18:40:41 +0100

    run without sampling with just mistral zero-shot

--- experiment-1.py
+++ experiment-1.py
@@ -15,7 +15,7 @@ import config
 import model
 
 
-NUM_SAMPLES = 5000
+NUM_SAMPLES = None
 
 
 # copied directly from experiment-1-shallow.py
@@ -241,13 +241,13 @@ if __name__ == "__main__":
     # Parameters to test
     prompt_names = [
         "expt1-zero-shot.txt",
-        "expt1-few-shot.txt"
+#        "expt1-few-shot.txt"
     ]
 
     model_names = [
         "mistral",
-        "llama-2",
-        "gemma-3",
+#        "llama-2",
+#        "gemma-3",
     ]
 
     model_configs = {
