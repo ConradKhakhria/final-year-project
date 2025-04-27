612586ae86884f395802989db207921423a8d00a
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-27 22:53:20 +0100

    now the correct one has 100 samples

--- experiment-2.py
+++ experiment-2.py
@@ -396,7 +396,8 @@ if __name__ == "__main__":
             chunk_report_max_tokens=200,
             overall_report_max_tokens=2000,
             model_name=model_name,
-            model_id=model_id
+            model_id=model_id,
+            num_samples=100
         )
 
     exit()
@@ -412,8 +413,7 @@ if __name__ == "__main__":
             chunk_report_max_tokens=200,
             overall_report_max_tokens=2000,
             model_name=model_name,
-            model_id=model_id,
-            num_samples=100
+            model_id=model_id
         )
 
     exit()
