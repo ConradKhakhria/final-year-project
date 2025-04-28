1294db932b9500f49859f489856497eaff314933
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-28 13:00:23 +0100

    record output text

--- experiment-2.py
+++ experiment-2.py
@@ -205,6 +205,10 @@ class Experiment2:
             }
         )
 
+        with open(config.RESULTS_DIR / "text-output.txt", "w") as f:
+            for i, o in enumerate(outputs):
+                print(f"=== output {i} ===:\n{o}\n\n")
+
         return model.extract_json(outputs, {
             "label": None,
             "evidence": None,
@@ -394,8 +398,8 @@ if __name__ == "__main__":
     # List LLMs to sample
     model_ids = {
         "mistral": "mistralai/Mistral-7B-Instruct-v0.3",
-        "llama-2": "meta-llama/Llama-2-7b-chat-hf",
-        "deepseek": "deepseek-ai/deepseek-llm-7b-chat"
+#        "llama-2": "meta-llama/Llama-2-7b-chat-hf",
+#        "deepseek": "deepseek-ai/deepseek-llm-7b-chat"
     }
 
     for model_name, model_id in model_ids.items():
