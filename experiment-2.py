efa69b96176b2134745adbdc9ce0c57bb8005518
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-28 21:59:47 +0100

    set env properyl

--- experiment-2.py
+++ experiment-2.py
@@ -453,7 +453,7 @@ if __name__ == "__main__":
     hf_token_path = config.CODE_DIR / "hf-access-token.txt"
     if hf_token_path.exists():
         with open(hf_token_path) as f:
-            os.environ["HUGGINGFACE_TOKEN"] = f.read().strip()
+            os.environ["HF_TOKEN"] = f.read().strip()
 
     expt = Experiment2(42)
 
