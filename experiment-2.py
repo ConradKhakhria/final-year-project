ff6e93e1d0abb61a37edfcd066a9e68099c00815
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-04-29 17:33:27 +0100

    70b llama

--- experiment-2.py
+++ experiment-2.py
@@ -649,22 +649,18 @@ if __name__ == "__main__":
     expt = Experiment2(42)
 
     model_configs = {
-#        "mistral": {
-#            "model_id": "mistralai/Mistral-7B-Instruct-v0.3",
-#            "context_window": 32768
-#        },
-        "mixtral": {
-            "model_id": "mistralai/Mixtral-8x7B-v0.1",
+        "mistral": {
+            "model_id": "mistralai/Mistral-7B-Instruct-v0.3",
             "context_window": 32768
         },
-#        "llama-2": {
-#            "model_id": "meta-llama/Llama-2-7b-chat-hf",
-#            "context_window": 4096
-#        },
-#        "deepseek": {
-#            "model_id": "deepseek-ai/deepseek-llm-7b-chat",
-#            "context_window": 4096
-#        }
+        "llama-2": {
+            "model_id": "TheBloke/Llama-2-70B-AWQ",
+            "context_window": 4096
+        },
+        "deepseek": {
+            "model_id": "deepseek-ai/deepseek-llm-7b-chat",
+            "context_window": 4096
+        }
     }
 
     
