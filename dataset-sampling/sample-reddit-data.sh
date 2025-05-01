b4836f5e2400a8922161b67e4147b0c5976d7617
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-05-01 04:19:29 +0100

    Now only does posts

--- dataset-sampling/sample-reddit-data.sh
+++ dataset-sampling/sample-reddit-data.sh
@@ -8,11 +8,11 @@ sudo apt install -y python3 python3-pip aria2 zstd
 echo "[2/4] Installing Python packages..."
 pip3 install zstandard pandas pyarrow
 
-echo "[3/4] Downloading March 2025 Reddit data via torrent..."
+echo "[3/4] Downloading March 2025 Reddit submissions via torrent..."
 wget -O reddit.torrent https://academictorrents.com/download/69d5e046e15c02182430879f50d62b18fe1404fb.torrent
 
 mkdir -p reddit_data
-aria2c --dir=reddit_data --seed-time=0 reddit.torrent
+aria2c --dir=reddit_data --seed-time=0 --select-file=2 reddit.torrent
 
 echo "[4/4] Running parallel sampling script..."
 python3 sample_reddit_parallel.py
