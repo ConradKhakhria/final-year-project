cc84b2980eaf9e34fe39fd2f5c4375fe1c7b7dbf
Conrad Khakhria <conradjkhakhria@gmail.com>
2025-05-01 04:16:31 +0100

    hmm

--- dataset-sampling/sample-reddit-data.sh
+++ dataset-sampling/sample-reddit-data.sh
@@ -9,8 +9,10 @@ echo "[2/4] Installing Python packages..."
 pip3 install zstandard pandas pyarrow
 
 echo "[3/4] Downloading March 2025 Reddit data via torrent..."
+wget -O reddit.torrent https://academictorrents.com/download/69d5e046e15c02182430879f50d62b18fe1404fb.torrent
+
 mkdir -p reddit_data
-aria2c --dir=reddit_data --seed-time=0 reddit_2025_03.torrent
+aria2c --dir=reddit_data --seed-time=0 reddit.torrent
 
 echo "[4/4] Running parallel sampling script..."
 python3 sample_reddit_parallel.py
